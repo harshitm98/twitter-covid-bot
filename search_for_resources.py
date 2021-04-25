f3fb6e6c6cc880f422d902171a7390763f2f4c28
Harshit Maheshwari <harshitmah98@gmail.com>
2021-04-25 22:54:31 +0530

    Modified .gitignore

--- search_for_resources.py
+++ search_for_resources.py
@@ -6,6 +6,7 @@ from tweet_entity import TweetEntity
 import requests
 from config import FIREBASE_URL
 import json
+from time import sleep
 
 with open("cities", "r") as f:
     cities = f.read().split("\n")
@@ -29,15 +30,17 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 for city in cities:
     print("[*] Searching for resources in {}...".format(city))
     for keyword in keywords:
+        old_tweets = 0
         print("\t Looking for {}...".format(keyword), end=" -> ")
         query = city + " " + keyword + " " + search_keyword + " " + ignore_keywords + " " + ignore_bots
         tweets = api.search(query, count=100, result_type = "recent")
-        print("Found: {} tweets".format(len(tweets)))
+        print("Found: {} tweets".format(len(tweets)), end = " ")
         for tweet in tweets:
             found_additional_keywords = [keyword]
             if "retweeted_status" not in tweet._json.keys():
                 tweet_id = tweet._json["id"]
                 if is_exists_in_database(tweet_id):
+                    old_tweets += 1
                     continue
                 tweet_time = tweet._json["created_at"]
                 tweet_text = tweet._json["text"]
@@ -50,3 +53,5 @@ for city in cities:
                     if additional_keyword in tweet_text and additional_keyword not in found_additional_keywords:
                         found_additional_keywords.append(additional_keyword)
                 upload_data(TweetEntity(tweet_id, city, ", ".join(found_additional_keywords), tweet_link, tweet_time))
+                sleep(0.5)
+        print("-> New Tweets: {}".format(len(tweets) - old_tweets))
