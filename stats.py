6eaab988f24568f7c64ad1f907b54f50f2c318c1
Harshit Maheshwari <harshitmah98@gmail.com>
2021-04-27 22:10:03 +0530

    Updated to new database

--- stats.py
+++ stats.py
@@ -1,16 +1,17 @@
 import requests
-from config import FIREBASE_URL, FIREBASE_REPLIED_URL
+from config import FIREBASE_NEW_URL, FIREBASE_NEW_REPLIED_URL
 import json
 
 def get_resources_database_size():
-    r = requests.get(FIREBASE_URL)
+    r = requests.get(FIREBASE_NEW_URL)
     if r.status_code != 200:
+        print("Error occured!" + str(r.content))
         return 0
     database = json.loads(r.text)
     return len(list(database.keys()))
 
 def get_replies_database_size():
-    r = requests.get(FIREBASE_REPLIED_URL)
+    r = requests.get(FIREBASE_NEW_REPLIED_URL)
     if r.status_code != 200:
         return 0
     database = json.loads(r.text)
