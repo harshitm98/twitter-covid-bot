9040749dd793f67c25a17fa53b58c3b47c719601
Harshit Maheshwari <harshitmah98@gmail.com>
2021-05-08 01:12:36 +0530

    Adds mention to reply

--- mention_to_reply.py
+++ mention_to_reply.py
@@ -1,18 +1,31 @@
 import tweepy
 import requests
-from config import FIREBASE_NEW_URL, FIREBASE_REPLIED_URL, FIREBASE_NEW_REPLIED_URL
 from auth import authenticate, authenticate_another, authenticate_search
+from add_to_db import get_database
 
-auth_reply = authenticate()
+with open("cities", "r+") as f:
+    cities = f.read().split("\n")
+    cities = cities[:30]
+
+keywords = ['plasma', 'ventilator', 'remdesivir', 'icu', 'hospital beds', 'oxygen']
+
+auth_reply = authenticate_another()
 api_reply = tweepy.API(auth_reply, wait_on_rate_limit=True)
 
-def get_mentions(since_id=0):
+def get_mentions(api, since_id=0):
     # new_since_id = since_id
     tweets = api_reply.mentions_timeline(tweet_mode="extended")
     for tweet in tweets:
         print("Username: {}".format(tweet.user.screen_name))
         print("Tweet id: {}".format(tweet.id))
         print("Tweet text: {}".format(tweet.full_text))
+        for city in cities:
+            if city in tweet.full_text:
+                for keyword in keywords:
+                    if keyword in tweet.full_text:
+                        database = get_database(city, keyword)
+                        break
+                break
         try:
             print("In reply to id: {}".format(tweet.in_reply_to_status_id))
         except:
@@ -20,4 +33,4 @@ def get_mentions(since_id=0):
         print("***")
         
         
-get_mentions()
+get_mentions(api_reply)
