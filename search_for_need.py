bcb049f85ca38cc7a05f0c26b052bb080764fe53
Harshit Maheshwari <harshitmah98@gmail.com>
2021-05-08 01:18:17 +0530

    Removes tweets from replies that have same content

--- search_for_need.py
+++ search_for_need.py
@@ -5,15 +5,21 @@ import json
 from dateutil.parser import parse
 from add_to_db import upload_replied, get_database, if_already_replied
 from time import sleep
+from re import sub
 
 with open("cities", "r") as f:
     cities = f.read().split("\n")
     cities = cities[:30]
 
-keywords = ['oxygen', 'remdesivir', 'icu', 'hospital beds', 'plasma', 'ventilator'] 
+keywords = ['ventilator','icu','remdesivir',  'plasma', 'hospital beds',  'oxygen'] 
 search_keyword = "required OR leads OR need"
 ignore_bots = "-ShariqueAly -findthecolors"
 filter_retweets = "-filter:retweets"
+
+def remove_usernames(tweet_text):
+    regex_string_for_username = r"(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)"
+    modified_tweet_text = sub(regex_string_for_username, "", tweet_text).replace("   ", " ").replace("  ", " ")
+    return modified_tweet_text
 
 auth_search = authenticate_search()
 api_search = tweepy.API(auth_search, wait_on_rate_limit=True)
@@ -40,24 +46,28 @@ for city in cities:
                 if if_already_replied(str(tweet_id)) or str(tweet_id) in replied_in_this_session:
                     continue
                 tweet_text = tweet._json["text"]
+                tweet_text = remove_usernames(tweet_text)
                 tweet_time = tweet._json["created_at"]
                 print("[-] Found a tweet with tweet id: {}".format(tweet_id), end=" -> ")
                 reply_string = ""
                 reply_string += "Here's a list of recent resources based on your keywords:\r\n"   
                 count = 0
+                list_tweet_text = []
                 for found_tweet in available_data:
                     if count >= 5:
                         break
-                    if keyword == "plasma" and count == 4:
-                        reply_string += "Find more donors at http://friends2support.org\r\n"
+                    if found_tweet["tweet_text"] in list_tweet_text:
+                        continue
+                    list_tweet_text.append(found_tweet["tweet_text"])
                     reply_string += found_tweet["tweet_link"] + "\r\n"
                     count += 1
                 # twitter_query = "https://twitter.com/search?q=" + city + "+verified+" + keyword + "+-needed+-required+-leads&f=live" 
                 if count == 0:
                     print("No leads found for {}.".format(keyword))
                     continue
+                if keyword == "plasma":
+                    reply_string += "Find more donors at http://friends2support.org\r\n"
                 reply_string += "Find more resources at http://resourcesbot.surge.sh"
-                print("Reply string: \n{}".format(reply_string))
                 try:
                     if replied_count % 2 == 0:
                         api_reply.update_status(status = reply_string, in_reply_to_status_id = tweet_id, auto_populate_reply_metadata=True)
